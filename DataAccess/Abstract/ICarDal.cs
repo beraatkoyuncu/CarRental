cc118aef8d64b58895b1af7d6d363afb2aa68a36
CASPER <CASPER@DESKTOP-4NI1NQ8>
2021-09-02 13:06:10 +0300

    Add project files.

---
+++ DataAccess/Abstract/ICarDal.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Conrerte;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //Dal : DataAccessLayer - veri erisim katmani (Dao olarak da kullanılıyor)
+    //Car tablosunun veri erisim katmani
+    public interface ICarDal
+    {
+        //interface metotlari default public
+
+        List<Car> GetAll();
+        void Add(Car car);
+        void Update(Car car);
+        void Delete(Car car);
+        List<Car> GetById(int Id);
+
+        List<Car> GetAllByBrand(int BrandId);//arabalari markaya göre filtrele
+
+    }
+}
---
+++ DataAccess/Conrerte/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,65 @@
+﻿using DataAccess.Abstract;
+using Entities.Conrerte;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Conrerte.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        //class'ın içinde ama metotların dışında olduğu için bu class'ın global değişkeni denir.bu yüzden _ ile tanımlanır.
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1, BrandId=1, CarName="Focus", ColorId=1, DailyPrice=200, ModelYear=2015, Description="."},
+                new Car{Id=2, BrandId=1, CarName="Fiesta", ColorId=1, DailyPrice=150, ModelYear=2014, Description="."},
+                new Car{Id=3, BrandId=2, CarName="Doblo", ColorId=1, DailyPrice=160, ModelYear=2016, Description="."},
+                new Car{Id=4, BrandId=2, CarName="Egea", ColorId=1, DailyPrice=180, ModelYear=2018, Description="."},
+                new Car{Id=5, BrandId=3, CarName="Megane", ColorId=1, DailyPrice=220, ModelYear=2019, Description="."},
+                new Car{Id=6, BrandId=3, CarName="Clio", ColorId=1, DailyPrice=170, ModelYear=2017, Description="."}
+            };
+        }
+
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetAllByBrand(int brandId)
+        {
+            return _cars.Where(c => c.BrandId == brandId).ToList();
+        }
+
+        public List<Car> GetById(int id)
+        {
+            return _cars.Where(c => c.Id == id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.CarName = car.CarName;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
